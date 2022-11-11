8518823118ce896f1b79b54f34f006eeb8bc9535
ethanz2003 <ethanz2003@gmail.com>
2022-11-11 16:58:52 -0500

    SchoolDegreeTemplates.json file update
    organized major info into school specific dictionaries.

--- DegreeTemplates/add_schools.py
+++ DegreeTemplates/add_schools.py
@@ -10,11 +10,19 @@ output = []
 
 # iterate through
 for school in schools:
+	schoolObj = {}
+	schoolObj["School Name"] = school["School Name"]
+	schoolObj["Majors"] = []
 	for school_major in school["Majors"]:
 		for major in data:
 			if major["Major"].find(school_major) != -1: #found the major
-				major["School"] = school["School Name"]
-				output.append(major)
+				schoolObj["Majors"].append(major)
+	output.append(schoolObj)
 
 with open("Data/SchoolDegreeTemplates.json", "w") as write_file:
 	json.dump(output, write_file, sort_keys=False, indent=2, ensure_ascii=False)
+
+# issue with repeated majors in DegreeTemplates.json
+# could look for repeated names and not do it or just repeat for multiple schools
+# Specifically Design, Innovation, and Society/Mechanical Engineering template repeats
+# Should be okay since this is a dual major between SoE and HASS
